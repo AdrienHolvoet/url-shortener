5d774e966f10576b470636cd7d183a14e72ede79
Adrien Holvoet <adrien@goensol.com>
2026-03-10 17:59:41 +0100

    :wrench: init env file and cors protection

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT ?? 3001;
+
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
 app.get("/health", (_req, res) => {
   res.send("ok");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
