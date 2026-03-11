9609f53d874697eae99c90931e16cdadcad17d49
Adrien Holvoet <adrien@goensol.com>
2026-03-11 09:10:21 +0100

    :sparkles: add rate limit middleware to protect public routes

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,20 +1,25 @@
 import { prisma } from "../lib/prisma";
-import { IRouter, Response, Router } from "express";
+import { IRouter, Request, Response, Router } from "express";
 import { router as urlsRouter } from "./urls";
+import { rateLimitMiddleware } from "../middlewares/rateLimit";
 
 export const router: IRouter = Router();
 
-router.use("/urls", urlsRouter);
+router.use("/urls", rateLimitMiddleware(), urlsRouter);
 
-router.get("/:slug", async (req, res: Response) => {
-  const url = await prisma.url.findUnique({ where: { slug: req.params.slug } });
+router.get(
+  "/:slug",
+  rateLimitMiddleware({ max: 200 }),
+  async (req: Request<{ slug: string }>, res: Response) => {
+    const url = await prisma.url.findUnique({ where: { slug: req.params.slug } });
 
-  if (!url) {
-    res.status(404).send("Ce lien n'existe pas");
-    return;
-  }
+    if (!url) {
+      res.status(404).send("Ce lien n'existe pas");
+      return;
+    }
 
-  prisma.click.create({ data: { urlId: url.id } }).then(() => {});
+    prisma.click.create({ data: { urlId: url.id } }).then(() => {});
 
-  res.redirect(302, url.originalUrl);
-});
+    res.redirect(302, url.originalUrl);
+  },
+);
